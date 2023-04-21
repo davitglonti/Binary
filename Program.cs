01a75b728acdea8a2e55e130d46962e31b6bd869
davitglonti <datiglonti@gmail.com>
2023-04-21 21:34:46 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//second project
+using System;
+
+public class Binarry
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter a Number");
+        var input = Console.ReadLine(); // read user input
+        int number = int.Parse(input!);
+        string binary = Convert.ToString(number,2);
+        Console.WriteLine($"The binary equivalent of {number} is {binary}");
+    }
+}
